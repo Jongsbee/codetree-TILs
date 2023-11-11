294afcb73dedd8167d57bb278c75b1c72e0ae43f
Jongsbee <104764340+Jongsbee@users.noreply.github.com>
2023-11-11 16:01:58 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231111/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int money;
+    cin >> money;
+    string answer;
+    if(money >= 3000)
+    {
+        answer = "book";
+    }
+    else if(money >= 1000)
+    {
+        answer = "mask";
+    }
+    else if(money >= 500)
+    {
+        asnwer = "pen";
+    }
+    else answer = "no";
+
+    cout << answer << endl;
+
+    return 0;
+}
