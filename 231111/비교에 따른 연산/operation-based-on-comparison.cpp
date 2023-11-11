3d7f80199f13e96d8460f599fcec618bfaa66629
Jongsbee <104764340+Jongsbee@users.noreply.github.com>
2023-11-11 15:55:57 +0900

    Title: 비교에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231111/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+        // 여기에 코드를 작
+    int a,b;
+    cin >> a >> b;
+
+    if(a>b)
+    {
+        cout << a*b << endl;
+    }
+    else
+    {
+        cout << b/a << endl;
+    }
+    return 0;
+}
