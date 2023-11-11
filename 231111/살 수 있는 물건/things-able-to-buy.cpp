120cf3499227639f74c3e6adcc58dc46c881f7d8
Jongsbee <104764340+Jongsbee@users.noreply.github.com>
2023-11-11 16:00:16 +0900

    Title: 살 수 있는 물건, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231111/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int money;
+    cin >> money;
+    string answer;
+    if(money >= 3000)
+    {
+        answer = "book";
+    }
+    else if(money >= 1000)
+    {
+        answer = "mask";
+    }
+    else answer = "no";
+
+    cout << answer << endl;
+
+    return 0;
+}
