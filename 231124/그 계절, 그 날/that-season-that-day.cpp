cdaa62a9c8916cb1de10b11fd3b405d439e801cb
Jongsbee <104764340+Jongsbee@users.noreply.github.com>
2023-11-24 15:11:19 +0900

    Title: 그 계절, 그 날, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231124/그 계절, 그 날/that-season-that-day.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    vector<int> months {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    int Y,M,D;
+    
+    cin >> Y >> M >> D;
+
+    // 윤년체크
+    if(Y%4 == 0)
+    {
+        if(Y%100 == 0)
+        {
+            if(Y%400 == 0) months[2] = 29;
+        }
+        else months[2] = 29;
+    }
+    
+    // 체크
+    if(D > months[M]) cout << -1 << endl;
+    else
+    {
+        if(M == 1 || M == 2 || M == 12) cout<< "Winter" << endl;
+        else if(M >= 3 && M <= 5) cout<< "Spring" << endl;
+        else if(M >= 6 && M <= 8) cout<< "Summer" << endl;
+        else cout<< "Fall" << endl;
+    }
+
+    return 0;
+}
