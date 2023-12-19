66122fd01be573448955cf1a3c9287d20b23c285
Jongsbee <104764340+Jongsbee@users.noreply.github.com>
2023-12-19 16:12:31 +0900

    Title: 문자열 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231219/문자열 정렬/string-sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    string s;
+    cin >> s;
+
+    sort(s.begin(), s.end());
+
+    cout << s << endl;
+    
+    return 0;
+}
