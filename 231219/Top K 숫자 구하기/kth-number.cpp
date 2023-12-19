01dbaba118973d0578840eec8fba59de0d663bb4
Jongsbee <104764340+Jongsbee@users.noreply.github.com>
2023-12-19 16:17:54 +0900

    Title: Top K 숫자 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231219/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> v(n);
+
+    for(int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    cout << v[k-1] << endl;
+    return 0;
+}
