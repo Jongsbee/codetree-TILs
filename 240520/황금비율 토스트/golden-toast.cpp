85f9642e7e7ecc7dabd873a052097498190a9022
Jongsbee <104764340+Jongsbee@users.noreply.github.com>
2024-05-20 16:12:43 +0900

    Title: 황금비율 토스트, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240520/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    cin.ignore();
+
+    vector<char> vec(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
+
+    char c, d;
+    vector<char>::iterator it = vec.end(); // 초기 이터레이터는 벡터 끝
+
+    for (int tc = 0; tc < m; tc++) {
+        cin >> c;
+
+        switch (c) {
+            case 'L':
+                if (it != vec.begin()) {
+                    --it;
+                }
+                break;
+
+            case 'P':
+                cin >> d;
+                it = vec.insert(it, d);
+                ++it;
+                break;
+
+            case 'D':
+                if(it != vec.end())
+                {
+                  it =  vec.erase(it);
+                }
+                break;
+
+            case 'R':
+                if (it != vec.end()) {
+                    ++it;
+                }
+                break;
+        }
+    }
+
+    for(const auto& c: vec)
+    {cout << c;}
+
+    cout << endl;
+
+
+    return 0;
+}
